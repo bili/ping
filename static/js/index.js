27b82e825aab5e1f07580c262523ce117a499c84
bili <panning20@gmail.com>
2015-10-21 17:11:04 +0800

    調整代碼，添加更多檢測

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,19 @@
 $(function() {
+    var inpect = new Inspect();
+    $('#platform').text(inpect.isMobile() ? 'Mobile' : 'PC');
+    $('#ua').text(inpect.getUA());
+    $('#cookieEnabled').text(inpect.isCookieEnabled() ? "已启用" : "已禁用");
     $('#connect').on('click', function() {
         var url = '/';
         var $btn = $(this);
         var $status = $('#connect-status');
         var timeout;
-        Inspect.ping(url, {
+        inpect.ping(url, {
             beforePing: function() {
                 var dots = ['', '.', '..', '...'];
                 var count = 0;
                 timeout = setInterval(function() {
-                    $status.html('连接中'+dots[count++%dots.length]);
+                    $status.html('连接中' + dots[count++ % dots.length]);
                 }, 350);
                 $btn.prop('disabled', true);
             },
@@ -19,7 +23,7 @@ $(function() {
                 if (status) {
                     $status.text('连接成功');
                 } else {
-                    $status.text('连接失敗'+req.status);
+                    $status.text('连接失敗（錯誤碼：' + req.status + '）');
                 }
             }
         });
--- static/js/inspect.js
+++ static/js/inspect.js
@@ -1,43 +1,78 @@
 (function() {
+    var Inspect = function() {
+        var _self = this;
+        this._ua = navigator.userAgent;
+        this._appVersion = navigator.userAgent;
+        this._browser = {
+            versions: function() {
+                var u = _self._ua;
+                return { //移动终端浏览器版本信息
+                    trident: u.indexOf('Trident') > -1, //IE内核
+                    presto: u.indexOf('Presto') > -1, //opera内核
+                    webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+                    mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                    android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+                    iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+                    iPad: u.indexOf('iPad') > -1, //是否iPad
+                    webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+                    wechat: u.indexOf('MicroMessenger') > -1 //是否微信內置瀏覽器
+                };
+            }(),
+            language: (navigator.browserLanguage || navigator.language).toLowerCase()
+        };
+    };
 
-    var Inspect = {
-        ping: function ping(url, opts) {
-            var isOk = false;
-            var maxCount = 3;
-            opts.delay = opts.delay || 1000;
-            if (opts.beforePing) opts.beforePing.call(this);
-            var timeout = setTimeout(function() {
-                var fn = arguments.callee;
-                if (isOk) {
-                    if (opts.afterPing) opts.afterPing.call(this, true);
-                    clearInterval(timeout);
-                } else {
-                    $.ajax({
-                        url: url,
-                        method: 'GET',
-                        cache: false,
-                        beforeSend: function() {},
-                        complete: function(XMLHttpRequest, textStatus) {
-                            if (XMLHttpRequest.status == 200) {
-                                isOk = true;
-                                fn.call(fn);
-                            } else {
-                                if (--maxCount < 1) {
-                                    if (opts.afterPing) opts.afterPing.call(this, false, XMLHttpRequest, textStatus);
-                                    return;
-                                }
-                                setTimeout(function() {
-                                    fn.call(fn);
-                                }, opts.delay);
+    Inspect.prototype.isMobile = function() {
+        if (this._browser.versions['mobile']) return true;
+        return false;
+    };
+
+    Inspect.prototype.getUA = function() {
+        return this._ua;
+    };
+
+    Inspect.prototype.isCookieEnabled = function() {
+        return navigator.cookieEnabled;
+    };
+
+    Inspect.prototype.ping = function(url, opts) {
+        var isOk = false;
+        var maxCount = 3;
+        opts.delay = opts.delay || 1000;
+        if (opts.beforePing) opts.beforePing.call(this);
+        var timeout = setTimeout(function() {
+            var fn = arguments.callee;
+            if (isOk) {
+                if (opts.afterPing) opts.afterPing.call(this, true);
+                clearInterval(timeout);
+            } else {
+                $.ajax({
+                    url: url,
+                    method: 'GET',
+                    cache: false,
+                    beforeSend: function() {},
+                    complete: function(XMLHttpRequest, textStatus) {
+                        if (XMLHttpRequest.status == 200) {
+                            isOk = true;
+                            fn.call(fn);
+                        } else {
+                            if (--maxCount < 1) {
+                                if (opts.afterPing) opts.afterPing.call(this, false, XMLHttpRequest, textStatus);
+                                return;
                             }
+                            setTimeout(function() {
+                                fn.call(fn);
+                            }, opts.delay);
                         }
-                    });
-                }
-            }, 1000);
-        }
-    }
+                    }
+                });
+            }
+        }, 1000);
+    };
 
     root = typeof exports !== "undefined" && exports !== null ? exports : window;
     root.Inspect = Inspect;
 
-}())
+}());
